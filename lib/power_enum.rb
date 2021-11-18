6eb1a308f1f146cd7ba88af8d3bed4c4f6c273d2
michaelhodgejr <mikeh@lightsky.com>
2021-11-18 08:20:40 -0600

    Update power_enum.rb

--- lib/power_enum.rb
+++ lib/power_enum.rb
@@ -9,34 +9,25 @@ require 'testing/rspec'
 # application is not the only user of the database, such as when it's used for
 # analytics or reporting.
 class PowerEnum < Rails::Engine
-
   config.autoload_paths << File.expand_path(File.join(__FILE__, "../"))
-
 
   initializer 'power_enum' do
     ActiveSupport.on_load(:active_record) do
-      Rails.application.reloader.to_prepare do
-        include PowerEnum::Enumerated
-        include PowerEnum::HasEnumerated
-      end
-      
-     
+      include PowerEnum::Enumerated
+      include PowerEnum::HasEnumerated
+
       ActiveRecord::Base.module_eval do
         class << self
-            prepend ::PowerEnum::Reflection
+          prepend ::PowerEnum::Reflection
         end
       end
 
       ActiveRecord::ConnectionAdapters.module_eval do
-              Rails.application.reloader.to_prepare do        
-                include PowerEnum::Schema::SchemaStatements
-              end
+        include PowerEnum::Schema::SchemaStatements
       end
 
       ActiveRecord::Migration::CommandRecorder.class_eval do
-        Rails.application.reloader.to_prepare do
-          include PowerEnum::Migration::CommandRecorder
-        end
+        include PowerEnum::Migration::CommandRecorder
       end
     end
 
